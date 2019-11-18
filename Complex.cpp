01885f4613a90b0e1df7f972bba1ea90550eeff3
Kris <kris_s2@outlook.com>
2019-11-17 16:52:45 -0800

    Marking display() function const  as it does not intend to change passed in state of the variables; Adding interface and implementation comments; Adding output file matching desired output for the assignment;

--- Complex.cpp
+++ Complex.cpp
@@ -10,30 +10,37 @@
 
 using namespace std;
 
+//ctor for the Complex number using MIL
 Complex::Complex(double real, double img) : real(real), img(img)
 {
 }
 
+//getter funciton for the real part fo the complex number;
 double Complex::getReal() const
 {
 	return real;
 }
 
+//setter function for the real part of the  complex  number;
 void Complex::setReal(const double &real) 
 {
 	this->real = real;
 }
 
+//getter function for the imaginary part of the compex number;
 double Complex::getImg() const 
 {
 	return img;
 }
 
+//setter function for the imaginary part of the  complex  number;
 void Complex::setImg(const double &img) 
 {
 	this->img = img;
 }
 
+//addition operation on the complex number;
+//taking Complex object and returning a pointer to a current Complex object;
 Complex& Complex::plusEq(const Complex& pC)
 {
 	this->real = real + pC.real;
@@ -41,6 +48,8 @@ Complex& Complex::plusEq(const Complex& pC)
 	return (*this);
 }
 
+//subtraction operation on the complex number;
+//taking Complex object and returning a pointer to a current Complex object;
 Complex& Complex::minusEq(const Complex& mC)
 {
 	this->real = real - mC.real;
@@ -48,7 +57,8 @@ Complex& Complex::minusEq(const Complex& mC)
 	return (*this);
 }
 
-void Complex::display(void) 
+//functon to display real and imaginary part of the Complex number;
+void Complex::display(void) const
 {
 	cout.precision(4);
 	cout << fixed << "(" << this->getReal() << " , " << this->getImg() << ")";
--- Complex.h
+++ Complex.h
@@ -9,20 +9,33 @@
 class Complex
 {
 private:
+	//real part of the Complex number;
 	double real;
+	//imaginary part of the Complex number;
 	double img;
 
 public:
+	//ctor for the Complex number;
 	Complex(double real=1.0, double img=0.0); // default constructor taking default values;
 
+	//getter function for the real part of the Complex number;
 	double getReal() const;
+	//setter function for the real part of the Complex number;
 	void setReal(const double &real);
 
+	//getter funciton for the imaginary part of the Complex number;
 	double getImg() const;
+	//setter function for the imaginary part of the Complex number;
 	void setImg(const double &img);
 
+	//addition function of the Complex number;
+	//takes a Compex number by reference and returns a reference to a Complex number;
 	Complex& plusEq(const Complex&);
-	Complex& minusEq(const Complex&);
-	void display(void);
 
+	//sumbtraction function of the Complex number;
+	//takes a Compex number by reference and returns a reference to a Complex number;
+	Complex& minusEq(const Complex&);
+
+	//Complex number display function;
+	void display(void) const;
 };
